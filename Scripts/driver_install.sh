3eb158cb71ce6d3a3e474fe25873704d55c8647e
Weijia Liang(WeLi) <weijia.liang>
2025-06-15 22:12:53 +0200

    update for installing  vcan

---
+++ Scripts/driver_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Installation of the necessary tools
+
+sudo apt update
+sudo apt install can-utils
+
+#Load kernel module (only once)
+sudo modprobe vcan
+
+#If you need to load can_raw (the protocol used by SocketCAN):
+sudo modprobe can
+sudo modprobe can_raw
+
+#Create virtual vcan interface
+sudo ip link add dev vcan0 type vcan
+sudo ip link set up vcan0
